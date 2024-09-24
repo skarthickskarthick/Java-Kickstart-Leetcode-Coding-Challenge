5be0a09f52f2671103d44baeea6862beb8e44c74
skarthickskarthick <139897562+skarthickskarthick@users.noreply.github.com>
2024-09-24 23:48:25 +0530

    Update 852. Peak Index in a Mountain Array.java

--- 852. Peak Index in a Mountain Array.java
+++ 852. Peak Index in a Mountain Array.java
@@ -1,35 +1,17 @@
-import java.util.*;
-public class solution {
-  public static int binarySearch(int[] arr, int low, int high, int key) {
-    while (low <= high) {
-
-      int mid = (low + high) / 2;
-      if (arr[mid] < key) {
-        low = mid + 1;
-      } else if (arr[mid] > key) {
-        high = mid - 1;
-      } else {
-        return mid;
-      }
-
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int st=0;
+        int sp=arr.length - 1;
+        while(st<=sp)
+        {
+          int  mid=st+(sp-st)/2;
+            if(arr[mid]>arr[mid+1])
+             sp=mid-1;
+             else
+                 st=mid+1;
+                  
+         }
+        
+        return st;
     }
-    return -1;
-  }
-  public static int findIndex(int[] arr, int key) {
-    int low = 0;
-    int high = 1;
-    while (arr[high] < key) {
-      low = high;
-      high = 2 * high;
-    }
-    return binarySearch(arr, low, high, key);
-  }
-  public static void main(String args[]) {
-    // Your code goes here
-    int[] arr = {3,5,7,9,10,90,100,130,140,160,170};
-
-    int ans = findIndex(arr, 130);
-
-    System.out.println("Element found at index " + ans);
-  }
 }
