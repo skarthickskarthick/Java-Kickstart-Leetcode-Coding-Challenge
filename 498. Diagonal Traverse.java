3da2c55ce301eeecb73780d0daee9b3916b91660
skarthickskarthick <139897562+skarthickskarthick@users.noreply.github.com>
2024-09-24 23:39:19 +0530

    Update 498. Diagonal Traverse.java

--- 498. Diagonal Traverse.java
+++ 498. Diagonal Traverse.java
@@ -1,15 +1,43 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-         for(int i=0;i<matrix.length;i++)
+    public int[] findDiagonalOrder(int[][] mat) {
+        int i=0,j=0,in=0;
+        boolean up=true;
+        int ans[]=new int[mat.length*mat[0].length];
+        while(i<mat.length&&j<mat[0].length)
         {
-            for(int j=i+1;j<matrix[0].length;j++)
+            if(up)
             {
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+               while(i>0&&j<mat[0].length-1)//traversing across the diagonal elements
+               {
+                ans[in++]=mat[i][j];
+                i--;
+                j++;
+               }
+               ans[in++]=mat[i][j];   //to overcome index out of bounds
+               if(j==mat[0].length-1)//direction change or turn
+               i++;
+               else
+               j++;
                
             }
-        }         //[[1,2,3],[4,5,6],[7,8,9]]-matrix
-                  // transpose- [[1,4,7],[2,5,8],[3,6,9]]
+            else
+            {
+
+                while(j>0&&i<mat.length-1)//traversing across the diagonal elements
+               {
+                ans[in++]=mat[i][j];
+                j--;
+                i++;
+               }
+               ans[in++]=mat[i][j];
+               if(i==mat.length-1)//direction change or turn
+               j++;
+               else
+               i++;
+              
+            }
+              up=!up;
+        }
+        return ans;
     }
 }
